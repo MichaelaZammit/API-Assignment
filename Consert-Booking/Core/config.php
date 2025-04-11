a855625d009c2da53c7d03601a64025cc4bd3b38
MichaelaZammit <michaela.zammit.f33220@mcast.edu.mt>
2025-04-11 18:33:16 +0200

    Change the file structure and Started code
    Started the code for the initialize and the config pages

---
+++ Consert-Booking/Core/config.php
@@ -0,0 +1,22 @@
+<?php 
+
+// Database details
+$host = "localhost";
+$db_name = "concert_finder";
+$username = "root";
+$password = "root";
+
+// PDO = PHP Data Objects
+// Used for Object Oriented Programming
+// Creating an 'object' makes our code much more organised
+$db = new PDO("mysql:host=localhost;dbname=".$db_name.";charset=utf8;",
+            $db_username,
+            $db_password);
+
+// set dome db attributes
+$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+$db->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, true);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+?>
